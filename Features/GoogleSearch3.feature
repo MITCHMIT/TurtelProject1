45fbcf310bb99cbbd4ce08ed04c08593a509e4e4
Md Mazed <Md Mazed@GWNC31514.lan>
2023-06-21 19:02:33 -0400

    These are the code for Turtle Project

---
+++ Features/GoogleSearch3.feature
@@ -0,0 +1,14 @@
+Feature:Google Search
+Scenario Outline:Simple Search
+
+Given I am on the google homepage
+When I enter search "<term>"
+And I click on the google search button
+Then I receive related search result
+
+   Examples:  
+
+     | term              |
+     | Quality Assurance |
+     | Software testing  |
+ 
---
+++ src/test/java/pages/GoogleHome.java
@@ -0,0 +1,45 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class GoogleHome {
+
+WebDriver driver;
+
+public GoogleHome (WebDriver driver) {
+
+this.driver = driver;
+PageFactory.initElements(driver,this);    
+
+}
+
+
+@FindBy(name="q")
+WebElement searchBox;
+//WebElement searchBox = driver.findElement(By.name("q"));
+
+//searchBox.sendKeys(string); 
+
+public void enterSearch(String string) {
+
+searchBox.sendKeys(string);
+}
+
+
+@FindBy(name="btnK")
+WebElement searchBtn;
+
+//WebElement searchBtn =driver.findElement(By.name("btnK"));
+
+public void clickSearch() {
+//searchBtn.click();
+	searchBtn.submit();
+}
+	
+}
+
+
